da41142e9efc09bd42eb2229d921f8802ccf2409
arun6783 <an6783@gmail.com>
2017-12-21 07:01:25 +0000

    following along..

---
+++ AarcharSweets/AarcharSweets/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AarcharSweets.Models;
+using AarcharSweets.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AarcharSweets.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult List()
+        {
+            var piesListVm = new PiesListViewModel
+            {
+                Pies = _pieRepository.Pies,
+                CurrentCategory = "Cheese Cakes"
+            };
+            return View(piesListVm);
+        }
+    }
+}
---
+++ AarcharSweets/AarcharSweets/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AarcharSweets.Models;
+
+namespace AarcharSweets.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public string CurrentCategory { get; set; }
+
+        public IEnumerable<Pie> Pies { get; set; }
+    }
+}
